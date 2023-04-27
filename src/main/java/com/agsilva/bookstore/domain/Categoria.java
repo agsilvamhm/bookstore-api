0169962ec462e9d9285f0b7f213cf311f62eefee
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2023-04-27 19:41:17 -0300

    Classes de domínio iniciadas

--- src/main/java/com/agsilva/bookstore/domain/Categoria.java
+++ src/main/java/com/agsilva/bookstore/domain/Categoria.java
@@ -1,3 +1,64 @@
-public class Categoria {
+package com.agsilva.bookstore.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+        private Integer id;
+        private String nome;
+        private String descricao;
+        private List<Livro> livros = new ArrayList<>();
+        public Categoria() {
+                super();
         }
+        public Categoria(Integer id, String nome, String descricao) {
+                this.id = id;
+                this.nome = nome;
+                this.descricao = descricao;
+        }
+
+        public Integer getId() {
+                return id;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+
+        public String getDescricao() {
+                return descricao;
+        }
+
+        public List<Livro> getLivros() {
+                return livros;
+        }
+
+        public void setId(Integer id) {
+                this.id = id;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public void setDescricao(String descricao) {
+                this.descricao = descricao;
+        }
+
+        public void setLivros(List<Livro> livros) {
+                this.livros = livros;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (!(o instanceof Categoria categoria)) return false;
+                return getId().equals(categoria.getId());
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(getId());
+        }
+}
--- src/main/java/com/agsilva/bookstore/domain/Livro.java
+++ src/main/java/com/agsilva/bookstore/domain/Livro.java
@@ -1,4 +1,65 @@
 package com.agsilva.bookstore.domain;
 
+import java.util.Objects;
+
 public class Livro {
+    private Integer id;
+    private String titulo;
+    private String texto;
+    private Categoria categoria;
+
+    public Livro(Integer id) {
+        super();
+    }
+
+    public Livro(Integer id, String titulo, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Livro livro)) return false;
+        return getId().equals(livro.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
