d2891ace29d81109f56282536104d668c22576c7
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2023-04-27 20:31:11 -0300

    Repositories Implementados

---
+++ src/main/java/com/agsilva/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.agsilva.bookstore.repositories;
+
+import com.agsilva.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
